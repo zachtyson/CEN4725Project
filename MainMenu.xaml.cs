338b8527efef26d3a7b9c3cc9257a3a041c71536
Zachary <tysonzachary@ufl.edu>
2023-03-23 05:13:21 -0400

    Created Main Menu

---
+++ MainMenu.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Microsoft.Samples.Kinect.BodyBasics
+{
+    public partial class MainMenu : Page
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
