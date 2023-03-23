2fdfce256b975cc012a89b07d840547ba6baa239
Zachary <tysonzachary@ufl.edu>
2023-03-23 05:13:13 -0400

    Created UpperWorkoutList.xaml

---
+++ UpperWorkoutList.xaml..cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Microsoft.Samples.Kinect.BodyBasics
+{
+    public partial class UpperWorkoutListPage : Page
+    {
+        public UpperWorkoutListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
