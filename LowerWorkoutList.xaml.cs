566ca0ba7e97f01bac19fdcd58e9f5653fe190b5
Zachary <tysonzachary@ufl.edu>
2023-03-23 05:15:16 -0400

    Created LowerWorkoutList.xaml

---
+++ LowerWorkoutList.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Microsoft.Samples.Kinect.BodyBasics
+{
+    public partial class LowerWorkoutList : Page
+    {
+        public LowerWorkoutList()
+        {
+            InitializeComponent();
+        }
+    }
+}
