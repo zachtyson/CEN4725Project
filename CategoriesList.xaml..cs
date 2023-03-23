e2960af1f29d1e2c53b66fe668f159461d2cd249
Zachary <tysonzachary@ufl.edu>
2023-03-23 05:13:30 -0400

    Created CategoriesList

---
+++ CategoriesList.xaml..cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Microsoft.Samples.Kinect.BodyBasics
+{
+    public partial class CategoriesListPage : Page
+    {
+        public CategoriesListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
